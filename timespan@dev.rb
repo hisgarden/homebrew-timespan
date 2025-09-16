167a4791977986d85db79e4452f9b5c3235d8f9d
Jin Wen <jin.wen@hisgarden.org>
2025-09-15 20:55:03 -0700

    fix: correct class name for timespan@dev formula

--- timespan@dev.rb
+++ timespan@dev.rb
@@ -1,4 +1,4 @@
-class TimespanAT1 < Formula
+class TimespanAtDev < Formula
   desc "A local time tracking application built with Rust (development version)"
   homepage "https://github.com/hisgarden/TimeSpan"
   url "https://github.com/hisgarden/TimeSpan.git", branch: "main"
